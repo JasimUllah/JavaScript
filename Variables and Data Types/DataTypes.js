b76b289c7f29690affd3d5ca5b4ab385a71b3235
JasimUllah <jasimullah3@gmail.com>
2025-01-22 22:06:02 +0500

    Primitive Data Types

---
+++ Variables and Data Types/DataTypes.js
@@ -0,0 +1,19 @@
+let age = 24;   // Numbers
+console.log(age);
+
+let price = 55.86;   // Numbers
+console.log(price);
+
+let fullName = "Jasim Ullah";   // String
+console.log(fullName);
+
+isFollowed = true   // Boolean
+
+let x;  //  Undefined
+
+let y = null    // Null (object)
+
+let bigInt = BigInt("12435678");    // BigInt
+console.log(bigInt)
+
+let aSymbol = Symbol("Hello!")
