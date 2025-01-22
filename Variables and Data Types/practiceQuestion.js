03dcec528746261fdf4f41a23a4fb39da7e7d69f
JasimUllah <jasimullah3@gmail.com>
2025-01-22 22:31:43 +0500

    Practice Questions

---
+++ Variables and Data Types/practiceQuestion.js
@@ -0,0 +1,19 @@
+const product = {
+    title: "Parker Ball Pen",
+    rating: 4,
+    offer: 5,
+    price: 250
+};
+
+console.log(product)
+
+
+const profile = {
+    userName: "Jasim Ullah",
+    isFollow: true,
+    followers: 100,
+    following: 5,
+    bio: "I am is Jasim Ullah and I am a system engineer"
+};
+
+console.log(profile)
