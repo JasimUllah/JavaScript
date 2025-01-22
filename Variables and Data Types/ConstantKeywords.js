0122fc1f665c7dadfccf7e663217afbbb3d53028
JasimUllah <jasimullah3@gmail.com>
2025-01-22 21:51:15 +0500

    Keywords

---
+++ Variables and Data Types/ConstantKeywords.js
@@ -0,0 +1,14 @@
+// The best practice to declare a variable is to use these keyword before declaring
+
+// 1. var: Variable can be re-declared and updated (A golbal scope varaible)
+// 2. let: Variable cannot be re-declared but can be updated (A block scope variable)
+//  3. const: Variable cannot be re-declard or updated (A block scope varable)
+
+console.log("Using Keywords");
+let fullName = "Jasim U";
+console.log(fullName);
+
+const PI = 3.14;    // const must be initialized it cannot be empty
+let r = 15;
+circleArea = PI * r;
+console.log(circleArea);
