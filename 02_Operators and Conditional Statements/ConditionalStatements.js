3e7f367dd676c6cb42fc026631d353bfc04992dd
JasimUllah <jasimullah3@gmail.com>
2025-01-23 21:02:52 +0500

    Conditional Statements

---
+++ 02_Operators and Conditional Statements/ConditionalStatements.js
@@ -0,0 +1,68 @@
+// Conditional Statements
+
+// If statement
+
+// let age = 16;
+
+// if(age >= 18) {
+//     console.log("You can vote");
+// }
+
+// if(age < 18) {
+//     console.log("You cannot vote");
+// }
+
+// ===========================================================================
+
+// if-else statement
+
+// let mode = "dark";
+// let color;
+
+// if(mode === "dark") {
+//     color = "black";
+// }
+
+// if(mode === "light") {
+//     color = "white";
+// }
+
+// if(mode === "dark") {
+//     color = "black";
+//     console.log("Dark mode applied");
+// }
+// else {
+//     color = "white";
+//     console.log("Light mode applied");
+// }
+
+// ===========================================================================
+
+// Even and Odd
+
+// let num = 15;
+
+// if(num%2 === 0) {
+//     console.log(num, "Even number");
+// }
+// else {
+//     console.log(num, "Odd number");
+// }
+
+// ===========================================================================
+
+// if else-if statemnet
+
+// let age = 25;
+
+// if (age >= 40) {
+//     console.log("Senior");
+// }
+// else if (age < 40 && age >= 25) {
+//     console.log("Middle");
+// }
+// else {
+//     console.log("Junior");
+// }
+
+// ===========================================================================
