fce2f28c520d53646dc2fe941027bfcb20c89592
JasimUllah <jasimullah3@gmail.com>
2025-01-23 18:33:17 +0500

    Operators and Conditional Statements

---
+++ Operators and Conditional Statements/Operators.js
@@ -0,0 +1,20 @@
+// Arithmetic operators
+
+let a = 5;
+let b = 2;
+
+console.log("a =", a, "b =", b);
+console.log("a + b = ", a+b);   // Addition
+console.log("a - b = ", a-b);   // Subtraction
+console.log("a * b = ", a*b);   // Multiplication
+console.log("a / b = ", a/b);   // Division
+
+console.log("a % b = ", a%b);   // Modulus finds remider
+console.log("a ** b = ", a**b); // Exponentiation a^b
+
+// Uniary Operators
+// a = a + 1;
+a++;
+console.log("a = ", a);
+a--;
+console.log("a = ", a);
