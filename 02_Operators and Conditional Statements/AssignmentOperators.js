e6756ab6b604ff66227f3739a4f6457cad261f3f
JasimUllah <jasimullah3@gmail.com>
2025-01-23 18:43:14 +0500

    Assignment Operatots

---
+++ 02_Operators and Conditional Statements/AssignmentOperators.js
@@ -0,0 +1,22 @@
+// Assignment Operators
+
+let a = 5;
+let b = 2;
+
+// a += 4; // a = a + 4
+// console.log("a = ", a) // 9
+
+// a -= 7;
+// console.log("a = ", a)
+
+// a *= 4
+// console.log("a = ", a)
+
+// a /= 4
+// console.log("a = ", a)
+
+// a %= 4
+// console.log("a = ", a)
+
+a **= 4
+console.log("a = ", a)
