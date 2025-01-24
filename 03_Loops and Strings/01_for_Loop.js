af4c08caf845175e2f48ab14f6d1de5d01a85271
JasimUllah <jasimullah3@gmail.com>
2025-01-24 14:40:07 +0500

    for loop

--- 03_Loops and Strings/01_for_Loop.js
+++ 03_Loops and Strings/01_for_Loop.js
@@ -1,5 +1,16 @@
 console.log("Jasim Ullah");
 
+
 for (let i = 1; i <= 5; i++) {
-    console.log("Hello World!");
+    console.log(i, "Hello World!");
 }
+// console.log(i);     // Will give error because i is declared inside the loop
+
+
+// Calculate the sum of 1 to 5 numbers
+
+// let sum = 0;
+// for (let i = 1; i <= 5; i++) {
+//     sum += i;
+// }
+// console.log("sum =", sum);
