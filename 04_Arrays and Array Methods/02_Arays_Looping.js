502fd98055e789c6b1fe74c0651cda2b81fb73f9
JasimUllah <jasimullah3@gmail.com>
2025-01-25 19:00:30 +0500

    Arrays Looping

---
+++ 04_Arrays and Array Methods/02_Arays_Looping.js
@@ -0,0 +1,16 @@
+let heroes = ["Ironman", "Superman", "Batman", "Spiderman", "Hulk"];
+
+// Printing user for loop
+for (let index = 0; index < heroes.length; index++) {
+    console.log(heroes[index]);
+}
+console.log("\n")
+
+// Printing user for of loop
+
+let cities = ["Peshawar", "Karachi", "Lahore", "Islamabad", "Multan", "Faislaabad"];
+
+for (let city of cities) {
+    console.log(city);
+}
+console.log("\n")
