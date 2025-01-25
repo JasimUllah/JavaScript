9cd59e4e87e89e95e289ec693d2ab26327763984
JasimUllah <jasimullah3@gmail.com>
2025-01-25 18:49:57 +0500

    Arrays

---
+++ 04_Arrays and Array Methods/01_Arrays.js
@@ -0,0 +1,18 @@
+let marks = [92, 44, 61,77, 98];
+console.log(marks);
+console.log(marks.length);
+
+let heroes = ["Ironman", "Superman", "Hulk"];
+console.log(heroes);
+
+// ===============================================================================
+
+// Array Indices
+
+console.log(marks[1]);
+console.log(marks[4]);
+
+// Changing valuse of array indices
+
+marks[1] = 55;
+console.log(marks);
