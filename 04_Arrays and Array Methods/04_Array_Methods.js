95d404cf8968a3f0f4c3b292695cdc432352f3ca
JasimUllah <jasimullah3@gmail.com>
2025-01-25 20:29:35 +0500

    Array Methods

---
+++ 04_Arrays and Array Methods/04_Array_Methods.js
@@ -0,0 +1,10 @@
+let foodItems = ["potato", "apple", "grapes", "tomato"];
+console.log(foodItems);
+
+foodItems.push("chips");
+console.log(foodItems);
+
+foodItems.pop();
+console.log(foodItems);
+
+console.log(foodItems.toString());
