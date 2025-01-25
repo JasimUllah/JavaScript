3df7837a8a09ea69159bc46c4e973962ce7d67e0
JasimUllah <jasimullah3@gmail.com>
2025-01-25 21:16:37 +0500

    Array Methods

--- 04_Arrays and Array Methods/04_Array_Methods.js
+++ 04_Arrays and Array Methods/04_Array_Methods.js
@@ -1,4 +1,4 @@
-let foodItems = ["potato", "apple", "grapes", "tomato"];
+let foodItems = ["potato", "apple", "grapes", "tomato", "orange"];
 console.log(foodItems);
 
 foodItems.push("chips");
@@ -8,3 +8,8 @@ foodItems.pop();
 console.log(foodItems);
 
 console.log(foodItems.toString());
+
+console.log(foodItems.slice(1, 4));
+
+let arr = [1, 2, 3, 4, 5, 6];
+arr.splice(2, 2, 101, 103);
