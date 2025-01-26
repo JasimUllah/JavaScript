2391f97949dedf4b353571aea0bd34007f4c88cf
JasimUllah <jasimullah3@gmail.com>
2025-01-26 18:47:58 +0500

    Functions

--- 05_Funcrions and Methods/01_Functions.js
+++ 05_Funcrions and Methods/01_Functions.js
@@ -1,5 +1,22 @@
-function myFunction () {
-    console.log("Welcome here!");
-}
+// function myFunction () {
+//     console.log("Welcome here!");
+// }
+// myFunction();
 
-myFunction();
+// // Parameter: used in function definition
+// function myFunction1 (msg) {
+//     console.log(msg);
+// }
+// // Argument: used in function call
+// myFunction1("With parameter");
+
+// ===================================================================================
+
+// Create a function which will calculate the sum of two numbers
+function sum (a, b) {
+    // console.log(a + b);
+    add = a + b;
+    return add;
+}
+let val = sum(3, 4);
+console.log(val);
