1bc08d9755fefe805150db9dab87e55342d01ebf
JasimUllah <jasimullah3@gmail.com>
2025-01-26 20:00:33 +0500

    Arrow Functions

---
+++ 05_Funcrions and Methods/02_Arrow_Functios.js
@@ -0,0 +1,15 @@
+// sum functions using arrows
+
+function sum (a, b) {
+    return a + b;
+}
+
+// Arrow function of sum
+const arrowSum = (a, b) => {
+    console.log(a + b);
+}
+
+// Arrow function of Multiplication
+const arrowMult = (a, b) => {
+    return a * b;
+}
