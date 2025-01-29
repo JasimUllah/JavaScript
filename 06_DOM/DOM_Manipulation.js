5f80c13841037d5fe90f841c3a0336ecf613e8d4
JasimUllah <jasimullah3@gmail.com>
2025-01-29 21:33:00 +0500

    DOM Properties

--- 06_DOM/DOM_Manipulation.js
+++ 06_DOM/DOM_Manipulation.js
@@ -1,19 +1,33 @@
 // Accessing elements or tags of html by id
-let heading = document.getElementById("heading"); // Returns the value of h1
-console.dir(heading);
+// let heading = document.getElementById("heading"); // Returns the value of h1
+// console.dir(heading);
 
 // Accessing elements or tags of html by id
-let headings = document.getElementsByClassName("heading-class"); // Returns the value of h1
-console.dir(headings);
+// let headings = document.getElementsByClassName("heading-class"); // Returns the value of h1
+// console.dir(headings);
 
 // Accessing by tags
-let paras = document.getElementsByTagName("p");
-console.dir(paras);
+// let paras = document.getElementsByTagName("p");
+// console.dir(paras);
 
 // Accessing by querySelector, this will return the first element
-let firstelement = document.querySelector("p");
-console.dir(firstelement);
+// let firstelement = document.querySelector("p");
+// console.dir(firstelement);
 
 // Accessing by querySelectorAll, this will return all the element
-let allelements = document.querySelectorAll("p");
-console.dir(allelements);
+// let allelements = document.querySelectorAll("p");
+// console.dir(allelements);
+
+
+
+// Part 2 DOM Manipulation
+// Accessing attrbutes of tags
+let div = document.querySelector("div");
+console.dir(div);
+
+let id = div.getAttribute("id");
+console.log(id);
+
+// setting attributes
+let para = document.querySelector("p");
+console.log(para.setAttribute("class", "myClass"));
