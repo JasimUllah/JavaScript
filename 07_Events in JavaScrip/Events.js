4564fedf4c7b05bfa434c2c62f3ad89d95ad5560
JasimUllah <jasimullah3@gmail.com>
2025-01-30 23:33:11 +0500

    JS event objects

--- 07_Events in JavaScrip/Events.js
+++ 07_Events in JavaScrip/Events.js
@@ -1,13 +1,20 @@
 let btn1 = document.querySelector("#btn1");
 
-btn1.onclick = () => {
-    console.log("btn1 was clicked");
-    let a = 25;
-    a++;
-    console.log(a);
-}
+// btn1.onclick = () => {
+//     console.log("btn1 was clicked");
+//     let a = 25;
+//     a++;
+//     console.log(a);
+// }
+
+// Event Object
+btn1.onclick = (evnt) => {
+    console.log(evnt);
+    console.log(evnt.type);
+    console.log(evnt.target);
+};
 
 let box = document.querySelector(".box");
 box.onmouseover = () => {
     console.log("You are hovering over box");
-}
+};
