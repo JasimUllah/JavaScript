c42f1429f9c04d8d40b0211f44aa5d830ed619a6
JasimUllah <jasimullah3@gmail.com>
2025-02-01 22:14:30 +0500

    tic tac toe game

--- 08_Tic Tac Toe Game/app.js
+++ 08_Tic Tac Toe Game/app.js
@@ -4,9 +4,9 @@ let newGameBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
-let turnO = true; // playerX, playerO
+let turnO = true; //playerX, playerO
+let count = 0; //To Track Draw
 
-// 2D Array for the winning patterns
 const winPatterns = [
   [0, 1, 2],
   [0, 3, 6],
@@ -19,51 +19,59 @@ const winPatterns = [
 ];
 
 const resetGame = () => {
-    turnO = true;
-    enableBoxes();
-    msgContainer.classList.add("hide");
-}
+  turnO = true;
+  count = 0;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
 
-// Checks which box is clicked and mark it as X and O
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
     if (turnO) {
-      // playerO turn
+      //playerO
       box.innerText = "O";
       turnO = false;
     } else {
-      // playerX turn
+      //playerX
       box.innerText = "X";
       turnO = true;
     }
     box.disabled = true;
-    checkWinner();
+    count++;
+
+    let isWinner = checkWinner();
+
+    if (count === 9 && !isWinner) {
+      gameDraw();
+    }
   });
 });
 
-// Function for getting a winner disable all the buttons or boxes
+const gameDraw = () => {
+  msg.innerText = `Game was a Draw.`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
 const disableBoxes = () => {
-    for (let box of boxes) {
-        box.disabled = true;
-    }
-}
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
 
 const enableBoxes = () => {
-    for (let box of boxes) {
-        box.disabled = false;
-        box.innerText = "";
-    }
-}
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
 
-
-// Function for showing the winner
 const showWinner = (winner) => {
   msg.innerText = `Congratulations, Winner is ${winner}`;
   msgContainer.classList.remove("hide");
   disableBoxes();
 };
 
-// Function to check the winner
 const checkWinner = () => {
   for (let pattern of winPatterns) {
     let pos1Val = boxes[pattern[0]].innerText;
@@ -73,6 +81,7 @@ const checkWinner = () => {
     if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
       if (pos1Val === pos2Val && pos2Val === pos3Val) {
         showWinner(pos1Val);
+        return true;
       }
     }
   }
