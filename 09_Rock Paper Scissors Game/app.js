267a8dd56f178f3dc0fe314a5e31c53e9f0898fb
JasimUllah <jasimullah3@gmail.com>
2025-02-04 20:26:33 +0500

    Draw Game Condition

--- 09_Rock Paper Scissors Game/app.js
+++ 09_Rock Paper Scissors Game/app.js
@@ -3,9 +3,33 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
+// Function to generate a random number for computer
+const genCompChoice = () => {
+  const options = ["rock", "paper", "scissors"];
+  const randIdx = Math.floor(Math.random() * 3);
+  return options[randIdx];
+};
+
+const drawGame = () => {
+  console.log("Game was a draw");
+};
+
+// Function for user to select a choice
+const playGame = (userChoice) => {
+  console.log("Choice was clicked", userChoice);
+  const compChoice = getCompChoice();
+  console.log("comp choice = ", compChoice);
+
+  // Draw condition
+  if (userChoice === compChoice) {
+    drawGame();
+  }
+};
+
+// Dsiplaying the clicked choice
 choices.forEach((choice) => {
   choice.addEventListener("click", () => {
-    let userChoice = choice.getAttribute("id");
-    console.log("Choice was clicked", userChoice);
+    const userChoice = choice.getAttribute("id");
+    playGame(userChoice);
   });
 });
